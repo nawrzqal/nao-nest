91bebaca4484516b5f74b8af53f212b0ae3d2b2f
nawrzqal <kallnaorz@gmail.com>
2025-03-18 15:53:00 +0200

    Providers

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,45 +1,45 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { NinjasService } from './ninjas.service';
 import { json } from 'stream/consumers';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
+import { identity } from 'rxjs';
 
 @Controller('ninjas')
 export class NinjasController {
+    // dependency Injection
+    constructor(private readonly ninjaService: NinjasService) {}
+
+
+
     // GET/ninjas --> []
-    // للحصول على قائمة النينجا.
     @Get()
-    getNinjas(@Query('type')  type:string ){
-        return [{
-            type
-        }];
+    getNinjas(@Query('weapon') weapon: 'stars' | 'nunchucks' ) {
+        // const service = new NinjasService();
+        return this.ninjaService.getNinjas(weapon);
     }
+
     // GET/ninjas/:id --> { ... }
-    // للحصول على نينجا معين باستخدام ID.
     @Get(':id')
     getOneNinja(@Param('id') id:string ){
-        return {
-            id
-        };
+        return this.ninjaService.getNinja(+id); // + because it comes as string in link and we need number
     }
+
     // POST/ninjas
-    // لإنشاء نينجا جديد.
     @Post()
     createNinja(@Body() createNinjaDto: CreateNinjaDto){
-        return {
-            name: createNinjaDto.name
-        };
+        return this.ninjaService.createNinja(createNinjaDto);
     }
+
     // PUT/PATCH/ninjas/:id --> { ... }
-    // لتحديث معلومات نينجا معين.
     @Put(':id')
-    uodateNinja(@Param('id') id:string, @Body() createNinjaDto: CreateNinjaDto ){
-        return {
-            name : createNinjaDto.name
-        };
+    uodateNinja(@Param('id') id:string, @Body() updateNinjaDto: UpdateNinjaDto ){
+        return this.ninjaService.updateNinja(+id,updateNinjaDto);
     }
+
     // DELETE/ninjas/:id
-    // لحذف نينجا معين.
     @Delete(':id')
-    removeNinja(){
-        return {};
+    removeNinja(@Param('id') id:string){
+        return this.ninjaService.removeNinja(+id);
     }
 }
--- src/ninjas/ninjas.service.ts
+++ src/ninjas/ninjas.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
-
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 @Injectable()
-export class NinjasService {}
+export class NinjasService {
+    private ninjas = [
+        { id: 1, name: 'Naruto', weapon: 'stars' },
+        { id: 2, name: 'Sasuke', weapon: 'nunchucks' },
+    ];
+
+    getNinjas(weapon: 'stars' | 'nunchucks') {
+        if (weapon) {
+          return this.ninjas.filter(ninja => ninja.weapon === weapon);
+        }
+        return this.ninjas;
+    }
+
+    getNinja(id: number) {
+        const ninja = this.ninjas.find(ninja => ninja.id === id);
+        if (!ninja) {
+          throw new Error('Ninja not found');
+        }
+        return ninja;
+    }
+
+    createNinja(createNinjaDto: CreateNinjaDto) {
+        const newNinja = {
+          id: Date.now(),
+          ...createNinjaDto,
+        };
+        this.ninjas.push(newNinja);
+        return newNinja;
+    }
+
+    updateNinja(id: number, updateNinjaDto: UpdateNinjaDto) {
+        this.ninjas = this.ninjas.map( (ninja)=>{
+            if(ninja.id == id){
+                return { ...ninja,...updateNinjaDto };
+            }
+
+            return ninja;
+        })
+
+        return this.getNinja(id);
+    }
+
+    removeNinja(id: number){
+        const toBeRemoved = this.getNinja(id);
+        
+        this.ninjas = this.ninjas.filter( (ninja)=> ninja.id !== id );
+
+        return toBeRemoved;
+    }
+}

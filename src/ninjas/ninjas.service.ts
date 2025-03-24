b1e38716b286e2d884f2296e9224fd34de6617ce
nawrzqal <kallnaorz@gmail.com>
2025-03-24 16:21:49 +0200

    using the Ninja Enitity in ninjas.service methodes

--- src/ninjas/ninjas.service.ts
+++ src/ninjas/ninjas.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
 import { UpdateNinjaDto } from './dto/update-ninja.dto';
+import { Ninja } from './entities/ninja.entity';
 @Injectable()
 export class NinjasService {
-    private ninjas = [
+    private ninjas: Ninja[] = [
         { id: 1, name: 'Naruto', weapon: 'stars' },
         { id: 2, name: 'Sasuke', weapon: 'nunchucks' },
     ];
 
-    getNinjas(weapon: 'stars' | 'nunchucks') {
+    getNinjas(weapon: 'stars' | 'nunchucks'): Ninja[] {
         if (weapon) {
           return this.ninjas.filter(ninja => ninja.weapon === weapon);
         }
         return this.ninjas;
     }
 
-    getNinja(id: number) {
+    getNinja(id: number) : Ninja {
         const ninja = this.ninjas.find(ninja => ninja.id === id);
         if (!ninja) {
           throw new Error('Ninja not found');
@@ -23,7 +24,7 @@ export class NinjasService {
         return ninja;
     }
 
-    createNinja(createNinjaDto: CreateNinjaDto) {
+    createNinja(createNinjaDto: CreateNinjaDto): Ninja {
         const newNinja = {
           id: Date.now(),
           ...createNinjaDto,
@@ -32,7 +33,7 @@ export class NinjasService {
         return newNinja;
     }
 
-    updateNinja(id: number, updateNinjaDto: UpdateNinjaDto) {
+    updateNinja(id: number, updateNinjaDto: UpdateNinjaDto): Ninja {
         this.ninjas = this.ninjas.map( (ninja)=>{
             if(ninja.id == id){
                 return { ...ninja,...updateNinjaDto };
@@ -44,7 +45,7 @@ export class NinjasService {
         return this.getNinja(id);
     }
 
-    removeNinja(id: number){
+    removeNinja(id: number): Ninja {
         const toBeRemoved = this.getNinja(id);
         
         this.ninjas = this.ninjas.filter( (ninja)=> ninja.id !== id );
